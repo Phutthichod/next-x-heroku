263241e6eff0ddc9fc7157d8dffbf282182b2128
Phutthichod <pin2017create@outlook.com>
2021-01-07 15:43:53 +0700

    update credential

--- pages/code/index.js
+++ pages/code/index.js
@@ -1,8 +1,15 @@
 import { useRouter } from 'next/router'
-import { useLayoutEffect, useState } from 'react'
+import { useLayoutEffect, useEffect, useState } from 'react'
 function Calendar() {
     const router = useRouter()
     const [eventList, setEventList] = useState([])
+    const lineRichMenuChange = () => {
+        fetch("https://api.line.me/v2/bot/user/{userId}/richmenu/{richMenuId}", {
+            method: "post"
+        }).then(res => res.json()).then(res => {
+            console.log(res)
+        })
+    }
     const submit = () => {
         if (router.query.code) {
             fetch("/api/hello", {
@@ -20,7 +27,22 @@ function Calendar() {
             })
         }
     }
-    console.log("render");
+    useEffect(async () => {
+        // fetch("/api/hello").then(res => res.json()).then(res => {
+        //   console.log(res)
+        // })
+        const { default: liff } = await import("@line/liff");
+        await liff.init({
+            liffId: "1655538913-PnDo5YK0" // Use own liffId
+        })
+
+        if (liff.isLoggedIn()) {
+
+            //   loadData()
+        }
+
+
+    }, [])
     return (
         <>
             <h2>nocnoc@mail.com</h2><button onClick={submit}>accept</button>
