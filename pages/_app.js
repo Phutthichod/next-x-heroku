01db4e7feee83b67b2e079281775637270f473f2
Phutthichod <pin2017create@outlook.com>
2021-01-07 12:15:00 +0700

    first commit 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  fetch("/api/hello").then(res => res.json()).then(res => {
+    console.log(res)
+  })
   return <Component {...pageProps} />
 }
 
