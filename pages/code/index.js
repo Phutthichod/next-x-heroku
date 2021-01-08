d17c0c10290200505773dfe942e5998d05b85c8b
Phutthichod <pin2017create@outlook.com>
2021-01-08 07:46:42 +0700

    first commit 2

--- pages/code/index.js
+++ pages/code/index.js
@@ -10,15 +10,15 @@ function Calendar() {
     const lineRichMenuChange = () => {
         let data = JSON.parse(localStorage.getItem("profile"))
         alert(data.userId, data.displayName)
-        let url = `https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`
-        let token = `7vFL8nhVmzvjohBD38AGXAYZfhe+6BMF3syevcddi4rUu8QlHEsrDK4dCIgv+WQsPhJXFZx1HIv6HHoeqhoRliOEmhx+hQ3nYV7TRYcoz76XVbqqMBOKsGILcG41KJUYEB+tVG5ar9tIBaZMtFNZ5gdB04t89/1O/w1cDnyilFU=`
-        const config = {
-            headers: { Authorization: `Bearer ${token}` }
-        };
+        // let url = `https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`
+        // let token = `7vFL8nhVmzvjohBD38AGXAYZfhe+6BMF3syevcddi4rUu8QlHEsrDK4dCIgv+WQsPhJXFZx1HIv6HHoeqhoRliOEmhx+hQ3nYV7TRYcoz76XVbqqMBOKsGILcG41KJUYEB+tVG5ar9tIBaZMtFNZ5gdB04t89/1O/w1cDnyilFU=`
+        // const config = {
+        //     headers: { Authorization: `Bearer ${token}` }
+        // };
 
-        const bodyParameters = {
+        // const bodyParameters = {
 
-        };
+        // };
         // Axios.post(
         //     url,
         //     bodyParameters,
