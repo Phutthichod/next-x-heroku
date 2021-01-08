3b65190e5ed67b7736ab40d34a4a5683f5191dc2
Phutthichod <pin2017create@outlook.com>
2021-01-08 07:17:22 +0700

    update line liff

--- pages/code/index.js
+++ pages/code/index.js
@@ -8,8 +8,8 @@ function Calendar() {
     // const [profile, setProfile] = useState({})
     const lineRichMenuChange = () => {
         let data = JSON.parse(localStorage.getItem("profile"))
-        alert(data.userId)
-        fetch(`https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`, {
+        alert(data.userId, data.displayName)
+        fetch(`https://api.line.me/v2/bot/user/U760192fc8a7be79e319da15255296557/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`, {
             method: "post",
             headers: new Headers({
                 'Authorization': 'Bearer  ' + "7vFL8nhVmzvjohBD38AGXAYZfhe+6BMF3syevcddi4rUu8QlHEsrDK4dCIgv+WQsPhJXFZx1HIv6HHoeqhoRliOEmhx+hQ3nYV7TRYcoz76XVbqqMBOKsGILcG41KJUYEB+tVG5ar9tIBaZMtFNZ5gdB04t89/1O/w1cDnyilFU=",
@@ -19,7 +19,7 @@ function Calendar() {
             alert("success")
             // liff.closeWindow()
         }).catch(err => {
-            alert("catch ")
+            alert(err)
         })
     }
     const submit = () => {
