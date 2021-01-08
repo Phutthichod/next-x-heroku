1967eda018c603cbe6fc49248cee588375300da6
Phutthichod <pin2017create@outlook.com>
2021-01-08 09:04:28 +0700

    first commit 2

--- pages/api/calApiLine.js
+++ pages/api/calApiLine.js
@@ -1,12 +1,13 @@
 const request = require('request');
-export default async (req, res) => {
+export default (req, res) => {
     const data = req.body
-    await request.post(data.url, {
+    request.post(data.url, {
         'auth': {
             'bearer': data.token
         }
     }).on('response', function (err) {
-        res.statusCode = 200
-        res.json({ test: "123" })
+
     })
+    res.statusCode = 200
+    res.json({ test: "123" })
 }
