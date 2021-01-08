98936483e740bc01e2ee23288d112efa6be4540c
Phutthichod <pin2017create@outlook.com>
2021-01-08 09:10:44 +0700

    first commit 2

--- pages/code/index.js
+++ pages/code/index.js
@@ -12,7 +12,7 @@ function Calendar() {
     const lineRichMenuChange = () => {
         console.log("test")
         let data = JSON.parse(localStorage.getItem("profile"))
-        // alert(data.userId, data.displayName)
+        alert(data.userId, data.displayName)
         let url = `https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`
         let token = `7vFL8nhVmzvjohBD38AGXAYZfhe+6BMF3syevcddi4rUu8QlHEsrDK4dCIgv+WQsPhJXFZx1HIv6HHoeqhoRliOEmhx+hQ3nYV7TRYcoz76XVbqqMBOKsGILcG41KJUYEB+tVG5ar9tIBaZMtFNZ5gdB04t89/1O/w1cDnyilFU=`
         fetch("/api/calApiLine", {
@@ -57,15 +57,6 @@ function Calendar() {
             })
         }
     }
-    useEffect(async () => {
-        // await liff.init({
-        //     liffId: "1655554465-Wld494r2" // Use own liffId
-        // }).then(() => {
-        //     setWindowOff(() => liff.closeWindow())
-        // })
-
-        // lineRichMenuChange()
-    })
     return (
         <>
             <h2>nocnoc@mail.com</h2><button onClick={() => submit()}>accept</button>
