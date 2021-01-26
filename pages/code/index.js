f40081287b3493c32739f1f6c4aab8d843518fd2
Phutthichod <pin2017create@outlook.com>
2021-01-26 08:42:19 +0700

    update rich menu

--- pages/code/index.js
+++ pages/code/index.js
@@ -13,7 +13,7 @@ function Calendar() {
         console.log("test")
         let data = JSON.parse(localStorage.getItem("profile"))
         alert(data.userId, data.displayName)
-        let url = `https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-e419d3ad588ff46ccf001de031fdf94e`
+        let url = `https://api.line.me/v2/bot/user/${data.userId}/richmenu/richmenu-c463432b860cd993d02c2f9387b3fadc`
         let token = `7vFL8nhVmzvjohBD38AGXAYZfhe+6BMF3syevcddi4rUu8QlHEsrDK4dCIgv+WQsPhJXFZx1HIv6HHoeqhoRliOEmhx+hQ3nYV7TRYcoz76XVbqqMBOKsGILcG41KJUYEB+tVG5ar9tIBaZMtFNZ5gdB04t89/1O/w1cDnyilFU=`
         fetch("/api/calApiLine", {
             method: "post",
