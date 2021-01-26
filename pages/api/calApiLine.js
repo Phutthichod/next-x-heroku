98b9b8302489905b8bc949a044ba6592d64b3b07
Phutthichod <pin2017create@outlook.com>
2021-01-26 09:41:06 +0700

    update rich menu

--- pages/api/calApiLine.js
+++ pages/api/calApiLine.js
@@ -1,18 +1,13 @@
 const request = require('request');
-var fs = require('fs');
 export default (req, res) => {
-    fs.appendFile('./list_userlineId', req.body.url, function (err) {
-        if (err) throw err;
-        const data = req.body
-        request.post(data.url, {
-            'auth': {
-                'bearer': data.token
-            }
-        }).on('response', function (err) {
+    const data = req.body
+    request.post(data.url, {
+        'auth': {
+            'bearer': data.token
+        }
+    }).on('response', function (err) {
 
-        })
-        res.statusCode = 200
-        res.json({ test: "123" })
-    });
-
+    })
+    res.statusCode = 200
+    res.json({ test: "123" })
 }
